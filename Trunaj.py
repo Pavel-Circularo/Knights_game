3cfcb6e44cf43dcd7d8e2972658fc1e4cdc041c8
Václav Fiala <60704196+OpposedOak@users.noreply.github.com>
2020-11-17 10:34:54 +0100

    Add files via upload

---
+++ Trunaj.py
@@ -0,0 +1,42 @@
+def vytvor_rytire():
+    """
+    Vytvoř rytíře 1 (R1) - jméno, výdrž, zbroj
+    Vytvoř rytíře 2 (R2) - jméno, výdrž, zbroj
+                        
+    """
+
+def parametry_kola():
+    """
+    Pro oba rytíře:
+    Zvolit váhu dřevce
+    Zvolit rychlost koně
+    Zvolit pozici dřevce
+    Zvolit pozici štítu
+    """
+    
+def stret():
+    """
+    Zkontrolovat pozice dřevce R1 a R2
+    Zkontrolovat pozici štítu R1 a R2
+    Porovnat obranné a útočné číslo R1 a R2
+    Zkontrolovat výdrže
+    Určit vítěze
+    """
+    
+def turnaj():
+    def vytvor_rytire():
+    p =#pocet kol
+    
+    for i in range(1,p+1):
+        def parametry_kola():
+        def stret():
+        
+        
+    
+    
+
+        
+    
+    
+
+    
