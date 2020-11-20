489da5b9d6393a7eb17278fae7f3e815bb999dc7
Václav Fiala <60704196+OpposedOak@users.noreply.github.com>
2020-11-20 12:32:05 +0100

    Update Trunaj.py

--- Trunaj.py
+++ Trunaj.py
@@ -45,30 +45,78 @@ def vytvor_rytire():
     return Rytir(jmeno,uc,oc,s,pos_d,pos_s,unava)
 
 def stret(R1,R2):
-    R1.utok()
-    R1.obrana()
-    
-    R2.utok()
-    R2.obrana()
+    R1.utok(), R1.obrana()
+    R2.utok(), R2.obrana()
+    #Zasahy
+    Zasah_R1 = R2.s - R1.uc
+    Zasah_R2 = R1.s - R2.uc
+    #Blokovani stitem
+    R1_blok = 0
+    R2_blok = 0
 
     if R1.pos_s == R2.pos_d:
         print(f"Rytíř {R1.jmeno} odrazil útok štítem")
+        R1_blok = 1
+        
+    if R2.pos_s == R1.pos_d:
+        print(f"Rytíř {R2.jmeno} odrazil útok štítem")
+        R2_blok = 1
+        
     #Zasah R1    
-    if R1.uc > R2.oc:
-        R2.s = R2.s - R1.uc
+    if R1.uc > R2.oc and R2_blok == 0:
+        R2.s = Zasah_R1
     #Zasah R2    
-    elif R1.uc < R2.oc:
-        R1.s = R1.s - R1.uc
-    #Remiza - R1.uc == R2.oc or R2.uc == R1.uc 
+    elif R1.uc < R2.oc and R1_blok == 0:
+        R1.s = Zasah_R2
+    #Remiza - R1.uc == R2.oc or R2.uc == R1.uc, nebo oba rytiri utok odrazili 
     else:
         pass
-    print(R1.uc,R1.oc,R1.s,R2.uc,R2.oc,R2.s)
+    print(f"Rytíř {R1.jmeno} zaútočil silou: {R1.uc}. Výdrž po {p}. kole: {R1.s}")
+    print(f"Rytíř {R2.jmeno} zaútočil silou: {R2.uc}. Výdrž po {p}. kole: {R2.s}")
     
+#Určení vítěze    
+def vitez(R1,R2):
+    if R1.s > R2.s and R1.s > 0:
+        print(f"Zvítězil rytíř {R1.jmeno}")
+        
+    elif R2.s > R1.s and R2.s > 0:
+        print(f"Zvítězil rytíř {R2.jmeno}")
+        
+    elif R1.s == R2.s or (R1.s and R2.s) <= 0:
+        print("Remíza")
+        
+#Kontrola shození před koncem turnaje        
+def shozeni(R1,R2):
+    if R1.s <= 0 and R2.s > 0:
+        print(f"Rytíř {R1.jmeno} je shozen ze sedla v {p}. kole")
+        return True
+    
+    elif R2.s <= 0 and R1.s > 0:
+        print(f"Rytíř {R2.jmeno} je shozen ze sedla v {p}. kole")
+        return True
+        
+    elif R2.s <= 0 and R1.s <= 0:
+        print(f"Oba rytíři jsou shozeni ze sedla v {p}. kole")
+        return True
+    
+#Hlavní program   
+def turnaj():
+    global p 
+    p = 0
+    pocet_kol = 3
+    vytvor_rytire()
+    
+    while p < pocet_kol:
+        p += 1
+        stret(R1,R2)
+        
+        if shozeni(R1,R2) == True:
+            break
+        
+    vitez(R1,R2)
 
-R1 = vytvor_rytire()
-R2 = vytvor_rytire()
+turnaj()  
 
-stret(R1,R2)
     
     
 
