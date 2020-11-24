dfbfcea75bf0e482e1909f8daa56a7c7b2e069a5
Václav Fiala <60704196+OpposedOak@users.noreply.github.com>
2020-11-24 13:38:33 +0100

    Update Automaticka_varianta.py

--- Automaticka_varianta.py
+++ Automaticka_varianta.py
@@ -1,6 +1,5 @@
 import random as rd
 
-#Testovaci varianta bez přístupu uživatele
 class Rytir():
     def __init__(self,jmeno,uc,oc,s,pos_d,pos_s,unava):
         self.jmeno = jmeno
@@ -11,24 +10,82 @@ class Rytir():
         self.pos_s = pos_s
         self.unava = unava
         
-    def utok(self):
-        #Vyber drevce
-        #drevec = input ("Zvol si drevec: ")
+    def priprava(self):
+        #Parametry vybavení
         typ_drevce = {"lehky":[-5,4],"stredni":[-10,6],"tezky":[-15,10]}
-        #Rychlost kone
-        #rychlost = input("Zadej rychlost kone cval/klus/trysk: ")
-        kun = {"cval":[0,0],"klus":[-3,2],"trysk":[-5,3],}
-        #self.pos_d = input("Kam miris drevcem H,LR,PR,T: ")
-        self.pos_d = "LR"
-        #Vypocet utocneho cisla
-        self.uc = rd.randrange(1,(typ_drevce ["stredni"] [1]) + 1) + kun["klus"] [1]
-        #Vypocet unavy
-        self.s = self.s + typ_drevce ["stredni"] [0] + kun["klus"] [0] + self.unava 
+        kun = {"cval":[0,0],"klus":[-3,2],"trysk":[-5,3]}
+        #Vyber drevce
+        while True:
+            drevec = (input ("Zvol si drevec lehky/stredni/tezky: ")).lower()
+            if drevec not in typ_drevce.keys():
+                print("Spatne zadany drevec")
+                continue
+            else:
+                self.kostka_uc = typ_drevce [drevec] [1]
+                self.unava = self.unava + typ_drevce [drevec] [0]
+                break
+                
+        while True:
+            rychlost = (input("Zadej rychlost kone cval/klus/trysk: ")).lower()
+            if rychlost not in kun.keys():
+                print("Spatne zadana rychlost")
+                continue
+            
+            else:
+                self.rychlost_uc = kun [rychlost] [1]
+                self.unava = self.unava + kun [rychlost] [0]
+                break
+                
+        while True:
+            pozice_drevce = (input("Kam miris drevcem H,L,P,T:")).lower()
+            if pozice_drevce not in "h,l,p,t":
+                print("Spatne zadana pozice drevce")
+                continue
+            else:
+                self.pos_d = pozice_drevce
+                break
+                
+        while True:
+            pozice_stitu = (input("Pozice stitu H,L,P,T:")).lower()
+            if pozice_stitu not in "h,l,p,t":
+                print("Spatne zadana pozice stitu")
+                continue
+            else:
+                self.pos_s = pozice_stitu
+                break
         
+        #Vypocet unavy
+        self.s = self.s + self.unava
+       
+    def utok(self):
+        #Vypocet utocneho cisla
+        self.uc = rd.randrange(1,self.kostka_uc + 1) + self.rychlost_uc
+
     def obrana(self):
         self.oc = self.oc + rd.randrange(0,7)
-        #self.pos_s = input("Pozice stitu H,LR,PR,T: ")
-        self.pos_s = "H"
+        
+
+def vytvor_rytire():
+    jmeno = input("Jmeno: ")
+    s = 100
+    uc = 0
+    oc = 0
+    pos_d =""
+    pos_s = ""
+    typ_zbroje = {"zadna":[0,0], "kozena":[-3,1], "krouzkova":[-5,3],"platova":[-7,4]}
+    
+    while True:
+
+        zbroj = (input("Zadej druh zbroje - zadna,kozena,krouzkova,platova: ")).lower()
+        if zbroj not in typ_zbroje.keys():
+            print("Špatne zadana zbroj")
+            continue
+        else:
+            unava = typ_zbroje[zbroj][0]
+            oc = oc + typ_zbroje[zbroj][1]
+            break
+    
+    return Rytir(jmeno,uc,oc,s,pos_d,pos_s,unava)
   
 def stret(R1,R2):
     #Nastaveni utocnych a obrannych cisel
@@ -40,8 +97,6 @@ def stret(R1,R2):
     #Blokovani stitem
     R1_blok = 0
     R2_blok = 0
-    print(f"Utok R1: {R1.uc}" ,f"Obrana R1: {R1.oc}",f"Utok R2: {R2.uc}",f"Obrana R2: {R2.oc}")
-    print(48*"-")
 
     if R1.pos_s == R2.pos_d:
         print(f"Rytíř {R1.jmeno} odrazil útok štítem")
@@ -62,7 +117,7 @@ def stret(R1,R2):
         print(f"Rytíř {R2.jmeno} překonal silou: {R2.uc}, obranu soupeře: {R1.oc}.")
         
     if ((R1.uc  <= R2.oc) and (R2.uc <= R1.uc)) or (R1_blok == 1 and R2_blok == 1) :
-        print("Rytíři se střetli ubránili soupeři")
+        print("Rytíři se ubránili soupeři")
 
 #Určení vítěze    
 def vitez(R1,R2):
@@ -96,13 +151,17 @@ def turnaj():
     global p
     p = 0
     pocet_kol = 3
-    #vytvor_rytire():
-    R1 = Rytir("Alistar",0,0,50,"","",0)
-    R2 = Rytir("Duncan",0,0,50,"","",0)
+    R1 = vytvor_rytire()
+    R2 = vytvor_rytire()
+    # R1 = Rytir("Alistar",0,0,100,"","",0)
+    # R2 = Rytir("Duncan",0,0,100,"","",0)
     
     while p < pocet_kol:
         p += 1
         print(20*"-"+f" KOLO {p} "+20*"-")
+        
+        R1.priprava()
+        R2.priprava()
         stret(R1,R2)
         
         if shozeni(R1,R2) == True:
